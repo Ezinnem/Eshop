69822f4587adf2318066c6f81f521f2d23e9ea75
Nnamani Ezinne Martina <nnamani.ezinne@gmal.com>
2021-07-26 18:32:40 +0100

    Created the Rating component to dispaly the product ratings and the number of reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -6,62 +7,24 @@ function App() {
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="index.html">Shopper</a>
+            <a className="brand" href="/">Shopper</a>
         </div>
         <div>
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
         </div>
 
     </header>
-    <main>
-        <div className="row center">
-          {
-            data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                    <img className="meduim" src={product.image} alt="product" />
-                </a>
-                <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                        <span>
-                            <i className="fa fa-star"></i>
-                        </span>
-                    </div>
-                    <div className="price">
-                        $120
-                    </div>
-                </div>
-            </div>
-            ))
-          }
-            <div className="card">
-                <a href="product.html">
-                    <img className="meduim" src="./images/product-1.jpeg" alt="product" />
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>Nike slim gown</h2>
-                    </a>
-                    <div className="rating">
-                        <span>
-                            <i className="fa fa-star"></i>
-                        </span>
-                    </div>
-                    <div className="price">
-                        $120
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-    <footer className="row center">
-        All rights reserved
-    </footer>
-</div>
+          <main>
+              <div className="row center">
+                  {
+                      data.products.map((product) => (
+                          <Product key={ product._id} product={product}/>
+                      ))}
+              </div>
+          </main>
+          <footer className="row center">All rights reserve</footer>
+      </div>
   )
 }
 
